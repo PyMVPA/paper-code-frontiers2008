edc4c64cdefa68c7783ae38c07b4b1ecb000da31
Yaroslav Halchenko <debian@onerussian.com>
2008-07-07 17:54:35 -0400

    RF+NF: added really silly sweep through the classifiers to sense the crappiness of the data

--- eeg_fruend.py
+++ eeg_fruend.py
@@ -11,13 +11,14 @@
 
 __docformat__ = 'restructuredtext'
 
-from mvpa.base import verbose
-from mvpa.datasets.eep import EEPDataset
-#from mvpa.misc.eepbin import EEPBin
+from mvpa.suite import *
+#from mvpa.base import verbose
+#from mvpa.datasets.eep import EEPDataset
+##from mvpa.misc.eepbin import EEPBin
+#from mvpa.misc.plot.erp import *
 
-from mvpa.misc.plot.erp import *
 
-verbose.level = 3
+verbose.level = 4
 
 datapath = '../data/eeg.fruend/ga14/'
 
@@ -47,29 +48,10 @@ def loadData():
 
     return d
 
-if __name__ == '__main__':
-    # TODO we need to make EEPBin available from the EEPDataset
-    # DONE some basic assignment of attributes to dsattr
-
-    # XXX: many things look ugly... we need cleaner interface at few
-    # places I guess
-    ds = loadData()
-
-    SR = 1.0/ds._dsattr['eb_dt']        # sampling rate
-    pre = -ds._dsattr['eb_t0']          # data is already trials, this would correspond sec before onset
-    nchannels, spt = ds.mapper.mask.shape      # number of channels, samples per trial
-    post = spt * 1.0/ SR - pre # compute seconds in trials after onset
-    ch_map = dict(zip(ds._dsattr['eb_channels'], xrange(nchannels))) # map from channel name to index
-
-    #
-    # Just a simple example of ERP plotting
-    # 
-
-    ch_of_interest = ch_map['Pz']
-    #ch_mask = N.zeros(ds.mapper.mask.shape, dtype='b')
-
-    plot_ids = ( 'sm', 'sf', 'dm', 'df' )
-
+#
+# Just a simple example of ERP plotting
+#
+def plotERP():
     # Nice to see the truth behind the bars ;-)
     for errtype in ['std', 'ste']:
         fig = P.figure(facecolor='white', figsize=(8,8))
@@ -86,8 +68,8 @@ if __name__ == '__main__':
         #    our plots we see that first we get N-peak
         #  * ERPs in the article are nice and smooth ;-) here we see the reality. May be ERPs were
         #    computed at different samples rate, rereferenced?
-        for i, plot_id in enumerate(plot_ids):
-            l1, l2 = plot_id + 'o', plot_id + 'n'
+        for i, cond_prefix in enumerate(cond_prefixes):
+            l1, l2 = cond_prefix + 'o', cond_prefix + 'n'
             ds_1 = ds.selectSamples(ds.idsbylabels([label2id[l1]]))
             ds_2 = ds.selectSamples(ds.idsbylabels([label2id[l2]]))
 
@@ -109,3 +91,47 @@ if __name__ == '__main__':
     # remove ylim in plot_erps to get somewhat different result without need for this adjust
     #fig.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.1, wspace=0.5, hspace=0.65)
     P.show()
+
+def clfEEG_dummy():
+    #
+    # Simple classification. Silly one for now
+    #
+    verbose(1, "Sweeping through classifiers with odd/even splitter for generalization")
+    for clf in clfs['binary']:
+        cv = CrossValidatedTransferError(
+            TransferError(clf),
+            OddEvenSplitter(),
+            enable_states=['confusion', 'training_confusion'])
+        verbose(2, "Classifier " + clf.descr)
+        for cond_prefix in cond_prefixes:
+            l1, l2 = cond_prefix + 'o', cond_prefix + 'n'
+            ds_ = ds.selectSamples(ds.idsbylabels([label2id[l1], label2id[l2]]))
+            error = cv(ds_)
+            tstats = cv.training_confusion.stats
+            stats = cv.confusion.stats
+            verbose(3, "%s vs %s: Training: ACC=%.2g MCC=%.2g, Testing: ACC=%.2g MCC=%.2g" %
+                    (l1, l2, tstats['ACC'], tstats['MCC'][0],
+                     stats['ACC'], stats['MCC'][0]))
+
+
+if __name__ == '__main__':
+    # TODO we need to make EEPBin available from the EEPDataset
+    # DONE some basic assignment of attributes to dsattr
+
+    # XXX: many things look ugly... we need cleaner interface at few
+    # places I guess
+    ds = loadData()
+
+    SR = 1.0/ds._dsattr['eb_dt']        # sampling rate
+    pre = -ds._dsattr['eb_t0']          # data is already trials, this would correspond sec before onset
+    nchannels, spt = ds.mapper.mask.shape      # number of channels, samples per trial
+    post = spt * 1.0/ SR - pre # compute seconds in trials after onset
+    ch_map = dict(zip(ds._dsattr['eb_channels'], xrange(nchannels))) # map from channel name to index
+
+    ch_of_interest = ch_map['Pz']
+    #ch_mask = N.zeros(ds.mapper.mask.shape, dtype='b')
+
+    cond_prefixes = ( 'sm', 'sf', 'dm', 'df' )
+
+    #plotERP()
+    clfEEG_dummy()
