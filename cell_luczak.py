7a70d9da901a5cfe1a433a3263cb9ab3f85aed12
Yaroslav Halchenko <debian@onerussian.com>
2008-07-11 22:34:21 -0400

    loading LFP data + adding cmdline args

--- cell_luczak.py
+++ cell_luczak.py
@@ -16,6 +16,11 @@ from scipy.io import loadmat
 
 import os.path
 
+parser.option_groups = [optsCommon]
+optVerbose.default=2                    # for now
+parser.add_options([optWaveletFamily, optZScore])
+(options, files) = parser.parse_args()
+
 verbose.level = 4
 
 datapath = os.path.join(cfg.get('paths', 'data root', default='../data'),
@@ -26,13 +31,21 @@ verbose(1, 'Datapath is %s' % datapath)
 
 def loadData():
     filepath = datapath + 'AL22_psth400.mat'
-    verbose(1, "Loading Cell data from %s" % filepath)
+    verbose(1, "Loading Spike counts data from %s" % filepath)
     cell_mat = loadmat(filepath)
     samples =  cell_mat['tc_spk']
     labels = cell_mat['tc_stim']
+
+    filepath = datapath + 'tc_eeg_AL22.mat'
+    verbose(1, "Loading LFP data from %s" % filepath)
+    lfp_mat = loadmat(filepath)
+    tc_eeg = lfp_mat['tc_eeg']
+
     d = MaskedDataset(samples=samples, labels=labels)
+    d_lfp = MaskedDataset(samples=tc_eeg, labels=labels)
     coarsenChunks(d, nchunks=4)         # lets split into 4 chunks
-    return d
+    coarsenChunks(d_lfp, nchunks=4)
+    return d, d_lfp
 
 
 def clf_dummy(ds):
@@ -44,14 +57,21 @@ def clf_dummy(ds):
     dsc = removeInvariantFeatures(ds)
     verbose(2, "Removed invariant features. Got %d out of %d features" % (dsc.nfeatures, ds.nfeatures))
 
-    for clf_ in clfs['sg', 'svm', 'multiclass'] + clfs['gpr', 'multiclass'] + clfs['smlr', 'multiclass']:
-      for clf in [FeatureSelectionClassifier(
+    for clf_ in clfs['smlr', 'multiclass']:
+      #clfs['sg', 'svm', 'multiclass'] + clfs['gpr', 'multiclass'] + clfs['smlr', 'multiclass']:
+      for clf in [ FeatureSelectionClassifier(
+                     clf_,
+                     SensitivityBasedFeatureSelection(
+                       OneWayAnova(),
+                       FractionTailSelector(0.010, mode='select', tail='upper')),
+                     descr="%s on 1%%(ANOVA)" % clf_.descr),
+                   FeatureSelectionClassifier(
                      clf_,
                      SensitivityBasedFeatureSelection(
                        OneWayAnova(),
                        FractionTailSelector(0.10, mode='select', tail='upper')),
                      descr="%s on 10%%(ANOVA)" % clf_.descr),
-#                  clf_
+                  clf_
                   ]:
         cv = CrossValidatedTransferError(
             TransferError(clf),
@@ -73,19 +93,21 @@ def main():
 
     # XXX: many things look ugly... we need cleaner interface at few
     # places I guess
-    ds = loadData()
+    ds, ds_lfp = loadData()
 
-    do_wavelets = False                 # although it might come handy
-    if do_wavelets:
-        verbose(2, "Converting into wavelets")
+    # lets work on LFPs
+    ds = ds_lfp
+
+    if options.wavelet_family is not None:
+        verbose(2, "Converting into wavelets family %s."
+                % options.wavelet_family)
         ebdata = ds.mapper.reverse(ds.samples)
-        WT = WaveletTransformationMapper(dim=2, wavelet='db1')
+        WT = WaveletTransformationMapper(dim=2, wavelet=options.wavelet_family)
         ds_orig = ds
         ebdata_wt = WT(ebdata)
         ds = MaskedDataset(samples=ebdata_wt, labels=ds_orig.labels, chunks=ds_orig.chunks)
 
-    do_zscore = False
-    if do_zscore:
+    if options.zscore:
         verbose(2, "Z-scoring full dataset")
         zscore(ds, perchunk=False)
 
