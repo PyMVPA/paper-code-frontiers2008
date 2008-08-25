3b681f6928040182163fe25975fadffd3f9a1d37
Yaroslav Halchenko <debian@onerussian.com>
2008-08-25 13:19:05 -0400

    NF: adjustment of processing pipeline
    * downsample prior to selection of time of interest
    * removal of baseline for each sample (assumed that first 200ms were
      in each sample for baseline like  in the paper)
    * rescaling data to fit within [-1, 1] range
    * a bit different way to assign chunks (with the same values)

--- meg_rieger.py
+++ meg_rieger.py
@@ -5,13 +5,16 @@
 from mvpa.suite import *
 from scipy.signal import resample
 
-# set MVPA_PATHS_DATA_ROOT accordingly! 
+# set MVPA_PATHS_DATA_ROOT accordingly!
 dataroot = cfg.get('paths', 'data root', default='data/meg.rieger')
 
 conditions = {1: 'cosu', 2: 'fausu'}
 
 # time window of interest (in seconds; starting with epoch)
 toi = 0.8
+# baseline pre-event duration (in seconds; starting with epoch)
+tob = 0.2
+
 # sampling rate after preprocessing in Hz
 target_samplingrate = 80
 
@@ -19,25 +22,38 @@ target_samplingrate = 80
 def loadData(subj):
     datasets = []
 
-    # use to generate unique chunks for all samples
-    chunk_counter = 0
-
     for cond_id, cond in conditions.iteritems():
+        verbose(1, 'Loading data for condition %s' % cond)
         # for now just with tiny dataset
         meg = TuebingenMEG(os.path.join(dataroot,
                                         subj + 'cf-3f' + cond + '.dat.gz'))
 
-        # mimic data preprocessing from paper
+        # mimic data preprocessing from the paper
 
         # just select MEG channels
         data = meg.data[:, [i for i, v in enumerate(meg.channelids)
                                             if v.startswith('M')]]
-        verbose(1, 'Selected %i channels' % data.shape[1])
+        verbose(2, 'Selected %i channels' % data.shape[1])
+
+        # Filter/downsample
+        # low-pass filter / resample each channels timeseries
+        # cutoff frequency = target_samplingrate/2
+        data = resample(data,
+                        meg.ntimepoints * target_samplingrate / meg.samplingrate,
+                        window='ham', axis=2)
+        verbose(2, 'Downsampled data to %d Hz' % target_samplingrate)
+
+        # Consider first 200ms to be baseline, thus shift signal accordingly
+        nsamples_base = int(target_samplingrate * tob)
+        baseline = N.mean(data[:, :, :nsamples_base], axis=2)
+        # remove baseline
+        data = data - baseline[..., N.newaxis]
+        verbose(2, 'Removed %f sec baseline' % tob)
 
         # extract time window of interest (starting at first sample)
-        nsamples_toi = int(meg.samplingrate * toi)
+        nsamples_toi = int(target_samplingrate * toi)
         verbose(1, 'Only using first %i timepoints, corresponding to %.2f s' \
-                % (nsamples_toi, nsamples_toi / meg.samplingrate))
+                % (nsamples_toi, nsamples_toi / target_samplingrate))
 
         ### YOH: NOT SURE... and they did select only 600ms after
         ### onset for classification, first 200ms were used for
@@ -46,30 +62,51 @@ def loadData(subj):
         # keep sample, keep channels, select TOI
         data = data[:, :, :nsamples_toi]
 
-        # low-pass filter / resample each channels timeseries
-        # cutoff frequency = target_samplingrate/2
-        data = resample(data,
-                        nsamples_toi * target_samplingrate / meg.samplingrate,
-                        window='ham', axis=2)
+
 
         # no chunks specified, ie. each sample will be in its own chunks
         datasets.append(MaskedDataset(
                             samples=data, labels=cond_id,
-                            chunks=range(chunk_counter,
-                                         chunk_counter + data.shape[0])))
-
-        chunk_counter += data.shape[0]
+                            chunks=0))
 
     # merge all datasets
     dataset = datasets[0]
     for d in datasets[1:]:
         dataset += d
+    # set uniq chunk id per each sample
+    dataset.chunks = N.arange(dataset.nsamples)
 
     return dataset
 
 
 # load the only subject that we have
 data = loadData('vp02')
+
+# Simple preconditioning of the data
+
+# Just divide by max value among first 33 sensors (34 and 91 seems to
+# be bad, thus we need to exclude them)
+data.samples *= 1.0/N.max(N.abs(
+    data.mapper.reverse(data.samples)[:,:33,:]))
+
+# or zscore?
+# zscore(data, perchunk=False)
+
+# We might want to rechunk differently and coarsen the chunks
+if False:
+    data.chunks[data.labels==1] = range(len(data.idsbylabels(1)))
+    data.chunks[data.labels==2] = range(len(data.idsbylabels(2)))
+    coarsenChunks(data, 10)
+
+
+# Some basic generalization testing
+if False:
+    te = TransferError(SMLR(lm=0.01), enable_states=['confusion', 'training_confusion', 'samples_error'])
+    splt = NFoldSplitter(nperlabel='equal')
+                         #nrunspersplit=2), # increase to reasonable number
+    splits = [x for x in splt(data)]
+    trd, ted = splits[0]
+    error = te(ted, trd)
 
 # to get results from Figure2A
 cv2A = CrossValidatedTransferError(
@@ -88,6 +125,6 @@ cv2B = CrossValidatedTransferError(
 # zscore -- when every sample is in its own chunk 'perchunk' is pointless
 #zscore(data, perchunk=False)
 
-
+verbose(1, "Running cross-validation on SMLR")
 #cv2A(data)
-#cv2B(data)
+cv2B(data)
