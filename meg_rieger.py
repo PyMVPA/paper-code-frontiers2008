e8f796f2248e6f213396f7b2e7aa141305b8df25
Yaroslav Halchenko <debian@onerussian.com>
2008-09-08 16:08:26 -0400

    torturing MEG dataset
    added literal labels
    testing scaled C values
    etc

--- meg_rieger.py
+++ meg_rieger.py
@@ -17,9 +17,9 @@ if not locals().has_key('__IP'):
 else:
     class O(object): pass
     options = O()
-    options.wavelet_family = None
-    options.wavelet_decomposition = 'dwt'
-    options.zscore = False
+    options.wavelet_family = None#'db1'
+    options.wavelet_decomposition = 'dwp'
+    options.zscore = True
 
 verbose.level = 4
 
@@ -27,7 +27,7 @@ verbose.level = 4
 datapath = cfg.get('paths', 'data root', default='data/meg.rieger')
 verbose(1, 'Datapath is %s' % datapath)
 
-conditions = {1: 'cosu', 2: 'fausu'}
+conditions = {'false': 'cosu', 'unsure': 'fausu'}
 subj = 'vp02'
 # sampling rate after preprocessing in Hz
 target_samplingrate = 80
@@ -37,7 +37,7 @@ def loadData(subj):
     datasets = []
 
     for cond_id, cond in conditions.iteritems():
-        verbose(1, 'Loading data for condition %s' % cond)
+        verbose(1, 'Loading data for condition %s:%s' % (cond, cond_id))
         # for now just with tiny dataset
         meg = TuebingenMEG(os.path.join(datapath,
                                         subj + 'cf-3f' + cond + '.dat.gz'))
@@ -50,7 +50,9 @@ def loadData(subj):
 
         verbose(2, 'Selected %i channels' % data.shape[1])
 
-        datasets.append(ChannelDataset(samples=data, labels=cond_id,
+        datasets.append(ChannelDataset(samples=data,
+                            labels=[cond_id] * len(data),
+                            labels_map=True,
                             channelids=channelids, dt=1./meg.samplingrate,
                             t0=meg.timepoints[0]))
 
@@ -98,11 +100,18 @@ def loadData(subj):
         # hmm, the code below does a better job than coarsenChunks, wrt to
         # equalized distribution of samples....
         nchunks = 10
-        dataset.chunks[dataset.labels==1] = \
-            N.arange(N.sum(dataset.labels == 1)) % nchunks
-        dataset.chunks[dataset.labels==2] = \
-            N.arange(N.sum(dataset.labels == 2)) % nchunks
+        for l in dataset.uniquelabels:
+            dataset.chunks[dataset.labels==l] = \
+                N.arange(N.sum(dataset.labels == l)) % nchunks
 
+    # If we decide to do searchlight, so we get 'generalization' per
+    # each time point after onset, using all sensors
+    dataset.mapper.setMetric(
+        DescreteMetric(elementsize=[1, 100],
+                       # distance function should be some custom one
+                       # to don't count different time points at all
+                       distance_function=cartesianDistance))
+    
     return dataset
 
 
@@ -138,16 +147,93 @@ if __name__ == '__main__':
         ds.samples *= 1.0/N.max(N.abs(ds.O[:,:33,:]))
 
     print ds.summary()
+    # For now lets just cheat and do on the whole ds, although it
+    # doesnt bias selection much (if at all) if later on we just do
+    # LOO testing (instead of coarse chunks)
+    doSelectNFeaturesAnova = False
+    if doSelectNFeaturesAnova:
+        verbose(1, 'Cruel feature selection')
+        ss = SensitivityBasedFeatureSelection(
+            OneWayAnova(),
+            FractionTailSelector(0.20, mode='select', tail='upper')
+            #FixedNElementTailSelector(1, mode='select', tail='upper')
+            )
+        ds = ss(ds)[0]
+        print ds.summary()
 
     best = {}
-    for clf in clfs['linear', '!lars', '!blr']:#[:1]:#[::-1]:
+    # libsvr never converges for some reason
+    #for clf in clfs['linear', '!lars', '!blr', '!libsvr', '!meta']:#[:1]:#[::-1]:
+    for clf in [sg.SVM(kernel_type='linear')]:
         try:# since some evil nuSVMs might puke on infeasible default nu
             # to get results from Figure2A
+            # C=-2.0 gives 84% when properly scaled and 5% ANOVA voxels
+            clf = sg.SVM(kernel_type='linear')
+            clf.C = -1.0
+            #for C in [-10, -6, -5, -4, -3, -2, -1, -0.5]:
+            #clf.C = C
+            print clf.C
+            spl = ds.samplesperlabel
+            ratio = N.sqrt(float(spl[0])/spl[1])
+            #C = clf.C                   # take scalar value
+            #clf.C = C
+            clf.C = (C/ratio, C*ratio)
+
+            rfesvm = sg.SVM(kernel_type='linear')
+            rfesvm.C = clf.C
+
+            # This classifier will do RFE while taking transfer error to testing
+            # set of that split. Resultant classifier is voted classifier on top
+            # of all splits, let see what that would do ;-)
+            clf = \
+              SplitClassifier(                      # which does splitting internally
+               FeatureSelectionClassifier(
+                clf = clf,
+                feature_selection = RFE(             # on features selected via RFE
+                    sensitivity_analyzer=\
+                        rfesvm.getSensitivityAnalyzer(transformer=Absolute),
+                    transfer_error=TransferError(rfesvm),
+                    stopping_criterion=FixedErrorThresholdStopCrit(0.05),
+                    feature_selector=FractionTailSelector(
+                                       0.2, mode='discard', tail='lower'),
+                                       # remove 20% of features at each step
+                    update_sensitivity=False)),
+                    # update sensitivity at each step
+                descr='LinSVM+RFE(N-Fold,static)')
+
+            #rfesvm_split = SplitClassifier(rfesvm)
+            #clf = FeatureSelectionClassifier(
+            #  clf = clf,
+            #  feature_selection = RFE(             # on features selected via RFE
+            #      # based on sensitivity of a clf which does splitting internally
+            #      sensitivity_analyzer=rfesvm_split.getSensitivityAnalyzer(
+            #          transformer=Absolute),
+            #      transfer_error=ConfusionBasedError(
+            #         rfesvm_split,
+            #         confusion_state="confusion"),
+            #         # and whose internal error we use
+            #      feature_selector=FractionTailSelector(
+            #                         0.2, mode='discard', tail='lower'),
+            #                         # remove 20% of features at each step
+            #      update_sensitivity=False),
+            #      # update sensitivity at each step
+            #  descr='LinSVM+RFE(splits_avg, static)' )
+
+
+
             cv2A = CrossValidatedTransferError(
                       TransferError(clf),
                       NFoldSplitter(),
                       enable_states=['confusion', 'training_confusion'])
 
+            verbose(1, "Running cross-validation on %s" % clf.descr)
+            error2A = cv2A(ds)
+            verbose(2, "Figure 2A LOO performance:\n%s" % cv2A.confusion)
+            if best.get('2A', (100, None, None))[0] > error2A:
+                best['2A'] = (error2A, cv2A.confusion, clf.descr)
+
+
+            clf.C = C
             # to get results from Figure2B
             cv2B = CrossValidatedTransferError(
                       TransferError(clf),
@@ -155,13 +241,6 @@ if __name__ == '__main__':
                                     # increase to reasonable number
                                     nrunspersplit=4),
                       enable_states=['confusion', 'training_confusion'])
-
-
-            verbose(1, "Running cross-validation on %s" % clf.descr)
-            error2A = cv2A(ds)
-            verbose(2, "Figure 2A LOO performance:\n%s" % cv2A.confusion)
-            if best.get('2A', (100, None, None))[0] > error2A:
-                best['2A'] = (error2A, cv2A.confusion, clf.descr)
 
             error2B = cv2B(ds)
             verbose(2, "Figure 2B LOO performance:\n%s" % cv2B.confusion)
